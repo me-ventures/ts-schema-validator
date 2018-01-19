968cbcf00ecf7908c6ed80667e234e84a5ae44b3
eva <squidstock@hotmail.com>
2018-01-19 01:59:42 +0100

    move timeout setter to top level

--- test/unit/SchemaProviderTest.ts
+++ test/unit/SchemaProviderTest.ts
@@ -6,10 +6,10 @@ import * as TJS from "typescript-json-schema";
 import { SchemaProvider } from "../../src";
 
 describe(`SchemaProvider`, function(){
+    this.timeout(30000);
+
     describe(`getSchemaForSymbol`, function() {
         it(`should get a schema for a given symbol`, async function(){
-            this.timeout(30000);
-
             const sut = new SchemaProvider({
                 path: "test/samples/ISampleInterface.ts"
             });
