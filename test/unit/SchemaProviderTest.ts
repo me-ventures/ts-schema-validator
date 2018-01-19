14feaed1086068fb03b559d85d959b3db3f4785f
eva <squidstock@hotmail.com>
2018-01-19 21:07:17 +0100

    add (commented out) test for es6 / compilerOptions
    - currently we get a bunch of errors / can't find types / can't find files

--- test/unit/SchemaProviderTest.ts
+++ test/unit/SchemaProviderTest.ts
@@ -3,6 +3,8 @@ import {} from "mocha";
 import {} from "node";
 import * as sinon from "sinon";
 import { SchemaProvider } from "../../src";
+import { ISampleInterfaceWithEs6Features } from "../samples/ISampleInterfaceWithEs6Features";
+import { resolve } from "path";
 
 describe(`SchemaProvider`, function(){
     this.timeout(30000);
@@ -21,6 +23,24 @@ describe(`SchemaProvider`, function(){
             Assert.equal(schema.properties.numberThing.type, "number");
             Assert.equal(schema.properties.stringThing.type, "string");
         });
+
+        // @todo: make this test work (es6 features / compilerOptions)
+        // it(`should get a schema for a given symbol using es6 features`, async function(){
+        //     const compilerOptions = require(resolve("tsconfig.json")).compilerOptions;
+        //
+        //     const sut = new SchemaProvider({
+        //         path: "test/samples/ISampleInterfaceWithEs6Features.ts",
+        //         compilerOptions: compilerOptions,
+        //     });
+        //
+        //     const schema: object|any = sut.getSchemaForSymbol("ISampleInterfaceWithEs6Features");
+        //
+        //     Assert.isObject(schema);
+        //     Assert.equal(schema.$schema, "http://json-schema.org/draft-06/schema#");
+        //     Assert.equal(schema.properties.booleanThing.type, "boolean");
+        //     Assert.equal(schema.properties.numberThing.type, "number");
+        //     Assert.equal(schema.properties.stringThing.type, "string");
+        // });
     });
 });
 
