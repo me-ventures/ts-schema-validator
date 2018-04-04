8574da408bb1ad724a3515ff6afc5e3d2828ead8
eva <squidstock@hotmail.com>
2018-04-04 13:25:06 +0200

    generate draft06 schema directly instead of draft04 > draft06
    resolves #2

---
+++ test/samples/ISampleInterfaceWithOptionals.ts
@@ -0,0 +1,5 @@
+export interface ISampleInterfaceWithOptionals {
+    stringThing: string;
+    numberThing ?: number;
+    booleanThing ?: boolean;
+}
--- test/unit/SchemaProviderTest.ts
+++ test/unit/SchemaProviderTest.ts
@@ -22,25 +22,53 @@ describe(`SchemaProvider`, function(){
             Assert.equal(schema.properties.booleanThing.type, "boolean");
             Assert.equal(schema.properties.numberThing.type, "number");
             Assert.equal(schema.properties.stringThing.type, "string");
+
+            Assert.isArray(schema.required);
+            Assert.equal(schema.required.length, 3);
+            Assert.deepEqual(schema.required, [
+                "booleanThing",
+                "numberThing",
+                "stringThing"
+            ]);
         });
 
-        // @todo: make this test work (es6 features / compilerOptions)
-        // it(`should get a schema for a given symbol using es6 features`, async function(){
-        //     const compilerOptions = require(resolve("tsconfig.json")).compilerOptions;
-        //
-        //     const sut = new SchemaProvider({
-        //         path: "test/samples/ISampleInterfaceWithEs6Features.ts",
-        //         compilerOptions: compilerOptions,
-        //     });
-        //
-        //     const schema: object|any = sut.getSchemaForSymbol("ISampleInterfaceWithEs6Features");
-        //
-        //     Assert.isObject(schema);
-        //     Assert.equal(schema.$schema, "http://json-schema.org/draft-06/schema#");
-        //     Assert.equal(schema.properties.booleanThing.type, "boolean");
-        //     Assert.equal(schema.properties.numberThing.type, "number");
-        //     Assert.equal(schema.properties.stringThing.type, "string");
-        // });
+        it(`should get a schema for a given symbol with optionals`, async function(){
+            const sut = new SchemaProvider({
+                path: "test/samples/ISampleInterfaceWithOptionals.ts"
+            });
+
+            const schema: object|any = sut.getSchemaForSymbol("ISampleInterfaceWithOptionals");
+
+            Assert.isObject(schema);
+            Assert.equal(schema.$schema, "http://json-schema.org/draft-06/schema#");
+            Assert.equal(schema.properties.booleanThing.type, "boolean");
+            Assert.equal(schema.properties.numberThing.type, "number");
+            Assert.equal(schema.properties.stringThing.type, "string");
+
+            Assert.isArray(schema.required);
+            Assert.equal(schema.required.length, 1);
+            Assert.deepEqual(schema.required, [
+                "stringThing"
+            ]);
+        });
+
+        // @todo: figure out how we atcually want this to work (technically works right now, but not really..)
+        it(`should get a schema for a given symbol using es6 features`, async function(){
+            const compilerOptions = require(resolve("tsconfig.json")).compilerOptions;
+
+            const sut = new SchemaProvider({
+                path: "test/samples/ISampleInterfaceWithEs6Features.ts",
+                compilerOptions: compilerOptions,
+            });
+
+            const schema: object|any = sut.getSchemaForSymbol("ISampleInterfaceWithEs6Features");
+
+            Assert.isObject(schema);
+            Assert.equal(schema.$schema, "http://json-schema.org/draft-06/schema#");
+            Assert.equal(schema.properties.booleanThing.type, "boolean");
+            Assert.equal(schema.properties.numberThing.type, "number");
+            Assert.equal(schema.properties.stringThing.type, "string");
+        });
     });
 });
 
